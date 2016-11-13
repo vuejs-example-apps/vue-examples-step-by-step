c3a4eda2cd3a676305204dcce2a0b55b9915b06b
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-11-13 10:59:52 +0200

    code structure reorganization: component extraction, async template loading

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ This branch cointains some incrementably adoptable standalone Vue examples.
 See tags: @step0, @step0.1, @step1 —  and so on.
 
 We start with a simple HTML-embedded example, and gradually move to a fully-functional WebPack-based application.
+
+You will need some sort of static HTTP server to run some of the examples. You could use `http-server`:
+
+```bash
+# installation
+npm install -g http-server
+# run it with caching disabled
+http-server -c-1
+```
--- components/AuthorSignature.js
+++ components/AuthorSignature.js
@@ -1,13 +1,16 @@
-var AuthorSignature = {
-    template: '#author-signature-template',
-    props: ['name', 'avatar_url', 'status', 'gender'],
-    computed: {
-        avatar: function () {                            
-            return this.avatar_url ||
-                ((this.gender == 'female')
-                    ? '../img/avatars/default-female-avatar.jpg'
-                    : '../img/avatars/default-male-avatar.png')
-        }
-        }
-
-};
+var AuthorSignature = Vue.component('AuthorSignature', function (resolve, reject) {
+    ajax.get("/components/AuthorSignature.tpl.html", function (template_string) {
+        resolve({
+            template: template_string,
+            props: ['name', 'avatar_url', 'status', 'gender'],
+            computed: {
+                avatar: function () {                            
+                    return this.avatar_url ||
+                        ((this.gender == 'female')
+                            ? '/assets/img/avatars/default-female-avatar.jpg'
+                            : '/assets/img/avatars/default-male-avatar.png')
+                }
+            }
+        })
+    });
+});
